d09b8f1d6abd239331e83dbeaaee6e25f7bae793
evelyn <evelyn0401evelyn@gmail.com>
2024-10-13 22:34:00 +0800

    微調按鈕

--- src/components/CircleButtonComponent.vue
+++ src/components/CircleButtonComponent.vue
@@ -5,8 +5,8 @@
 <template>
     <a href="#" class="link">
 	<svg viewBox='0 0 200 200' width='200' height='200' xmlns='http://www.w3.org/2000/svg' class="link__svg" aria-labelledby="link1-title link1-desc">
-		<title id="link1-title">Come quick and click this</title>
-		<desc id="link1-desc">A rotating link with text placed around a circle with an arrow inside</desc>
+		<!-- <title id="link1-title">Come quick and click this</title>
+		<desc id="link1-desc">A rotating link with text placed around a circle with an arrow inside</desc> 不知用途先注解-->
 
 		<path id="link-circle" class="link__path" d="M 20, 100 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0" stroke="none" fill="none" />
 
