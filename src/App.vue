f2800417bb4971b944fd2de40ded5686027f9c11
evelyn <evelyn0401evelyn@gmail.com>
2024-10-13 20:46:35 +0800

    SendButtonComponent.vue製作送信按鈕組件

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
     import 'bootstrap/dist/css/bootstrap.min.css'
     import 'bootstrap/dist/js/bootstrap.min.js'
     import 'jquery/dist/jquery.min.js'
-import BudgetPlanView from './views/BudgetPlanView.vue';
 </script>
 
 <template>
-    <!-- <BudgetPlanView></BudgetPlanView> -->
     <RouterLink to="/budgetplan">圖表</RouterLink>
     <RouterView></RouterView>
 </template>
