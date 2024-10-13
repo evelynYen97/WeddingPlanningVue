a216e026b8af4d42f4d33ae16c29979adf914315
evelyn <evelyn0401evelyn@gmail.com>
2024-10-13 11:07:14 +0800

    在App.vue引用bootstrap5、icon及jquery，建立預算清單備用組件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+    import 'bootstrap/dist/css/bootstrap.min.css'
+    import 'bootstrap/dist/js/bootstrap.min.js'
+    import 'jquery/dist/jquery.min.js'
 </script>
 
 <template>
-
 </template>
 
 <style scoped>
