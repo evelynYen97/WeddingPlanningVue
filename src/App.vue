8fa08f685d51952db8a154b1fc7d1d62e4687aa4
evelyn <evelyn0401evelyn@gmail.com>
2024-10-13 21:10:07 +0800

    ClickButtonAComponent建立點擊按鈕

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
     import 'bootstrap/dist/css/bootstrap.min.css'
     import 'bootstrap/dist/js/bootstrap.min.js'
     import 'jquery/dist/jquery.min.js'
+import ClickButtonAComponent from './components/ClickButtonAComponent.vue';
 </script>
 
 <template>
     <RouterLink to="/budgetplan">圖表</RouterLink>
     <RouterView></RouterView>
+    <ClickButtonAComponent></ClickButtonAComponent>
 </template>
 
 <style scoped>
