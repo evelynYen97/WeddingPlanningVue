bf1672c4ec55b6f18d27ec73e7793a107486629f
evelyn <evelyn0401evelyn@gmail.com>
2024-10-14 17:52:53 +0800

    piechart數據可調用顯示

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,11 @@
     import 'bootstrap/dist/js/bootstrap.min.js'
     import 'jquery/dist/jquery.min.js'
 import BudgetChartComponent from './components/BudgetChartComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
     <RouterView></RouterView>
     <BudgetChartComponent></BudgetChartComponent>
-    <FooterComponent></FooterComponent>
 </template>
 
 <style scoped>
--- src/components/BudgetChartComponent.vue
+++ src/components/BudgetChartComponent.vue
@@ -8,102 +8,95 @@
   <script setup>
   import { onMounted, ref } from 'vue';
   import * as echarts from 'echarts';
-    const API_URL='https://localhost:7048/api/MemberBudgetItems'
-    //宣告圓餅圖資料
-    const pieData=ref([])
-    //宣告圖表
-    const pieChart = ref(null);
-    const barChart = ref(null);
-    //呼叫圓餅圖資料  待修改成傳入會員ID！！
-    const loadPieData=async()=>{
-        const response=await fetch(`${API_URL}/ForChart/1`)
-        const datas=await response.json()
-        console.log(datas)
-        pieData.value = datas
-        console.log(pieData.value)
-        pieChart.setOption({
-                series: [{
-                    data:pieData // 將獲取到的資料設置到圓餅圖中
-                }]
-            });
-    }
-    
-    
-    //圖表初始化設定
-    const initCharts = () => {
+
+const API_URL = 'https://localhost:7048/api/MemberBudgetItems';
+const pieData = ref([]); // Use ref to make pieData reactive
+const pieChart = ref(null);
+const barChart = ref(null);
+
+const loadPieData = async (memberId) => {
+    const response = await fetch(`${API_URL}/ForChart/${memberId}`);
+    const datas = await response.json();
+    pieData.value = datas; 
+    updateChart(); 
+};
+
+const updateChart = () => {
     const pieChartInstance = echarts.init(pieChart.value);
-    const barChartInstance = echarts.init(barChart.value);
-  
     const pieOption = {
         title: {
-                text: '類別預算',
-                subtext: '點擊查看細項',
-                left: 'center'
-            },
-            tooltip: {
-                trigger: 'item'
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left'
-            },
-            series: [{
-                name: '訂單類別',
-                type: 'pie',
-                radius: '50%',
-                data: [],
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
+            text: '類別預算',
+            subtext: '點擊查看細項',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [{
+            name: '訂單類別',
+            type: 'pie',
+            radius: '50%',
+            data: pieData.value, 
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
                 }
-            }]
-        };
-  
-        const barOption = {
-            title: {
-                text: '類別細項'
-            },
-            tooltip: {},
-            legend: {
-                data: ['金額']
-            },
-          xAxis: {
+            }
+        }]
+    };
+    pieChartInstance.setOption(pieOption);
+    return pieChartInstance; 
+};
+
+const initCharts = () => {
+    const barChartInstance = echarts.init(barChart.value);
+    const barOption = {
+        title: {
+            text: '類別細項'
+        },
+        tooltip: {},
+        legend: {
+            data: ['金額']
+        },
+        xAxis: {
             type: 'category',
             data: ['X', 'Y', 'Z', 'd']
-          },
-          yAxis: {
+        },
+        yAxis: {
             type: 'value'
-          },
-          series: [{
-            name:'金額',
+        },
+        series: [{
+            name: '金額',
             type: 'bar',
             data: [5, 10, 15, 7]
-          }]
-        };
-  
-        pieChartInstance.setOption(pieOption);
-        barChartInstance.setOption(barOption);
-  
-        pieChartInstance.on('click', (params) => {
-          const index = params.dataIndex;
-          const data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-          barChartInstance.setOption({
-            series: [{
-              data: data[index]
-            }]
-          });
-        });
-      };
-  
-    onMounted(()=>{
-      initCharts();
+        }]
+    };
 
-      loadPieData()
-      
+    barChartInstance.setOption(barOption);
+
+    const pieChartInstance = updatePieChart(); // Get the pie chart instance after updating it
+    pieChartInstance.on('click', (params) => {
+        const index = params.dataIndex;
+        const data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; // Example data
+        barChartInstance.setOption({
+            series: [{
+                data: data[index]
+            }]
+        });
     });
+};
+
+onMounted(() => {
+    const memberId = 2; // Replace with the actual member ID you want to use
+    loadPieData(memberId); // Pass the member ID to load the data
+    initCharts(); // Initialize the charts after loading data
+});
   </script>
   
   <style scoped>
