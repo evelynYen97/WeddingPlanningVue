050d92ccfbc3302ad056c013e5320868e5a5960e
evelyn <evelyn0401evelyn@gmail.com>
2024-10-15 21:19:55 +0800

    建立share_components 公用組件資料夾 移動文件

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
     import 'bootstrap/dist/css/bootstrap.min.css'
     import 'bootstrap/dist/js/bootstrap.min.js'
     import 'jquery/dist/jquery.min.js'
-import BudgetChartComponent from './components/BudgetChartComponent.vue';
+
 </script>
 
 <template>
     <RouterView></RouterView>
-    <BudgetChartComponent></BudgetChartComponent>
+    
+
 </template>
 
 <style scoped>
