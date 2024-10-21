83bcb6093f055a5fe9ba1b2c789959de6ee0b267
fcu-d0845487 <e67976964@gmail.com>
2024-10-21 16:58:31 +0800

    1021 重新合併

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,13 @@
 <script setup>
+import '@fortawesome/fontawesome-free/css/all.css';
 
 </script>
 
 <template>
-    <div class="footer-container"> 
-        <!-- 品牌區域開始 -->
-        <div class="section section-padding section-fluid-240 bg-custom">
+    <!-- footer-all start-->
+    <div> 
+        <!-- 圖標區域開始 -->
+        <div class="section section-padding section-fluid-240 brand-bg ">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-12">
@@ -13,35 +15,35 @@
                             <div class="brand-list d-flex justify-content-between flex-wrap">
                                 <!-- 品牌項目 -->
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
-                                    <div class="brand text-center">
+                                    <div class="brand">
                                         <a href="#">
                                             <img src="../assets/images/brand/client-logo-01.png" alt="logo image" class="img-fluid">
                                         </a>
                                     </div>
                                 </div>
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
-                                    <div class="brand text-center">
+                                    <div class="brand">
                                         <a href="#">
                                             <img src="../assets/images/brand/client-logo-02.png" alt="logo image" class="img-fluid">
                                         </a>
                                     </div>
                                 </div>
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
-                                    <div class="brand text-center">
+                                    <div class="brand">
                                         <a href="#">
-                                            <img src="../assets/images/brand/client-logo-03.png" alt="logo image" class="img-fluid">
+                                            <img src="../assets/images/brand/client-logo-01.png" alt="logo image" class="img-fluid">
                                         </a>
                                     </div>
                                 </div>
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
-                                    <div class="brand text-center">
+                                    <div class="brand">
                                         <a href="#">
                                             <img src="../assets/images/brand/client-logo-04.png" alt="logo image" class="img-fluid">
                                         </a>
                                     </div>
                                 </div>
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
-                                    <div class="brand text-center">
+                                    <div class="brand">
                                         <a href="#">
                                             <img src="../assets/images/brand/client-logo-05.png" alt="logo image" class="img-fluid">
                                         </a>
@@ -53,99 +55,102 @@
                 </div>
             </div>
         </div>
-        <!-- 品牌區域結束 -->
-
-        <div class="footer-section section">
+        <!-- 圖標區域結束 -->
+         
+         <!-- 資訊區域開始 -->
+        <div class="section footer-section ">
             <div class="container">
-                <div class="row mb-8 align-items-center">                
-                    <!-- 關於我們 -->
+                <div class="row">
+                    <!-- 關於我們 start-->
                     <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-6">
-                        <div class="footer-widget">
+                        <div class="footer-widget"> 
                             <h5 class="footer-widget-title">關於我們</h5>
                             <div class="footer-widget-content">
-                                <p class="about-text">我們是一家致力於提供高品質產品與服務的公司，旨在滿足客戶的需求。</p>
-                                <p class="about-text">自成立以來，我們秉持著創新、專業與誠信的理念，努力成為業界的領導者。</p>
+                                <!-- 關於資訊 start-->
+                                <div class="d-flex justify-content-between link-margin">
+                                    <!-- 左邊的連結 -->
+                                    <ul class="footer-links">
+                                        <li><a href="#">首頁</a></li>
+                                        <li><a href="#">聯絡我們</a></li>
+                                        <li><a href="#">常見問題</a></li>
+                                        <li><a href="#">隱私政策</a></li>
+                                        <li><a href="#">產品與服務</a></li>
+                                    </ul>
+
+                                    <!-- 右邊的連結 -->
+                                    <ul class="footer-links">
+                                        <li><a href="#">最新消息</a></li>
+                                        <li><a href="#">合作夥伴</a></li>
+                                        <li><a href="#">工作機會</a></li>
+                                        <li><a href="#">支援中心</a></li>
+                                        <li><a href="#">條款與條件</a></li>
+                                    </ul>
+                                </div>
+                                <!-- 關於資訊 end-->
+
+                                <!-- 社交媒體連結 start-->
+                                <div>
+                                    <ul class="social-links d-flex align-items-center justify-content-center mt-4">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                    </ul>
+                                </div>
+                                <!-- 社交媒體連結 end-->
+
                             </div>
                         </div>
                     </div>
+                    <!-- 關於我們 end-->
 
-
-                    <!-- 頁尾 logo -->
-                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-6 text-md-center">
+                    <!-- 頁尾 logo start-->
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-6">
                         <div class="footer-logo">
-                            <a href="index.html"><img src="../assets/images/logo/footer-logo.png" alt="Logo"></a>
+                            <a href="index.html"><img src="../assets/images/logo/weddingLogo.png" alt="Logo"></a>
                         </div>
                     </div>
+                    <!-- 頁尾 logo end-->
 
-                  <!-- 贊助我們 -->
+                    <!-- 贊助我們 start-->
                     <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-6">
-                        <div class="footer-widget">
+                        <div class="footer-widget sponsor">
                             <h5 class="footer-widget-title">贊助我們</h5>
                             <div class="footer-widget-content">
-                                <p class="about-text">如果您想支持我們的工作或有興趣贊助我們的項目，歡迎通過以下方式聯絡： 
-                                    <a href="mailto:support@example.com">Aurora Bliss@example.com</a>
-                                </p>
-                                <p class="about-text rem-margin-bottom">匯款資訊：</p>
-                                <ul class="about-text rem-padding-right ul-right-margin-top ul-no-style">
-                                    <li>銀行名稱：婚企銀行</li>
-                                    <li>帳號：001234567890</li>
-                                    <li>帳戶名稱：Aurora Bliss</li>
-                                </ul>
+                                <p class="sponsor-text">如果您願意支持我們的工作或贊助我們的項目，您可以透過以下方式聯絡我們：</p>
+                                <div class="bank-margin">
+                                    <ul class="bank-no-style">
+                                        <li>聯絡人：Aurora Bliss</li>
+                                        <li>電子郵件： <a href="mailto:AuroraBliss@gmail.com">AuroraBliss@gmail.com</a></li>
+                                        <li>銀行名稱：婚企銀行</li>
+                                        <li>帳號：001234567890</li>
+                                        <li>帳戶名稱：Aurora Bliss</li>
+                                    </ul>
+                                </div>  
                             </div>
                         </div>
                     </div>
+                    <!-- 贊助我們 end-->
 
                 </div>
                 
                 <!-- 版權信息 -->
-                <div class="row">
-                    <div class="col">
-                        <p class="copyright">© 2024 Aurora Bliss. All rights reserved.  </p>
+                <div class="row ">
+                    <div class="col ">
+                        <p class="copyright">© 2024 Aurora Bliss. All rights reserved.</p>
                     </div>
                 </div>
                 <!-- 版權信息結束 -->
             </div>
         </div>
+         <!-- 資訊區域結束-->
     
     </div>
+    <!-- footer-all end-->
 </template>
 
 <style lang="css" scoped>
 
 @import '../assets/css/FooterStyle.css';
-
-.footer-container {
-    display: flex; 
-    /* 使用 flexbox */
-    flex-direction: column; 
-    /* 確保 footer 內容按列排列 */
-    min-height: 100vh; 
-    /* 確保 footer 在頁面底部 */
-    justify-content: space-between; /* 確保內容分散在容器中 */
-    
-}
-
-.brand-list {
-    display: flex; 
-    justify-content: space-between; 
-    flex-wrap: wrap; 
-}
-
-.brand {
-    text-align: center; 
-    margin: 0 15px; 
-}
-
-.img-fluid {
-    max-width: 100%; 
-    height: auto; 
-}
-
-
-.bg-custom {
-    background-color: #F0F2F2; 
-}
-
 
 .footer-widget-title {
     font-size: 1.5rem; 
@@ -155,34 +160,102 @@
     text-align: center; 
 }
 
-.about-text {
-    font-size: 1rem; 
-    line-height: 1.6; 
+
+.footer-widget-content {
+    /* display: flex;    */
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+
+.brand {
+    text-align: center; 
+    margin: 0 15px; 
+}
+
+.brand-list {
+    display: flex; 
+    justify-content: space-between; 
+    flex-wrap: wrap; 
+}
+
+.brand-bg {
+    background-color: rgb(240, 242, 242); 
+}
+
+
+.img-fluid {
+    max-width: 100%; 
+    height: auto; 
+
+}
+
+
+.footer-links {
+    list-style: none; 
+    padding: 0; 
+}
+
+.footer-links li {
+    margin-bottom: 0.5rem; 
+}
+
+
+.social-links {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 10px;
+}
+
+.social-links li {
+    margin: 0 5px;
+}
+
+.social-links a {
+    color: #555;
+    font-size: 1.5rem;
+}
+
+.link-margin {
+    margin-left: 30px;
+    margin-right: 30px; 
+}
+
+.sponsor {
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%; /* 讓寬度占滿可用空間 */
+    max-width: 350px; /* 設定最大寬度 */
+}
+
+.sponsor-text {
     color: #555; 
-    margin: 0.5rem 0; 
     text-align: justify; 
+    margin-bottom: 0px;
+    
 }
-
-   
-.rem-margin-bottom {
-    margin-bottom: 0.2rem; 
-}
-
-.ul-right-margin-top {
-    text-align: right; 
-    width: 100%;
-    margin-top: 0.2rem; 
-}
-
-.rem-padding-right {
-    padding-right: 1rem; 
-}
-
-
-.ul-no-style {
+ 
+.bank-no-style {
     list-style-type: none; 
+    padding: 0;
+    margin: 0;
+    text-align: left; /* 讓文字左對齊 */
+    /* width: 100%;  */
+    /* 保證 ul 占滿整個父容器，實現左對齊 */
+}
+
+.bank-margin {
+    padding: 0;
+    display: flex;
+    justify-content: center; /* 水平置中 */
+    align-items: center; /* 垂直置中 */  
 }
 
 
 
+    
 </style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,201 @@
+<script setup>
+    import { onMounted } from 'vue';
+
+    onMounted(() => {
+    // You can add any JS logic here if needed
+    });
+</script>
+
+<template>
+  <div class="main-container">
+
+    
+    <div class="container my-5">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+        <img src="/src/assets/images/navbg/navImage.jpg" alt="婚禮" class="img-fluid rounded">
+        </div>
+        <div class="col-md-6 text-center text-md-start">
+        <h2 class="display-4">永恆的誓約</h2>
+        <h3>慶祝您的愛情</h3>
+        <p class="lead">
+            與我們的專家團隊一起發現完美的婚禮規劃體驗。從精心挑選場地到提供專業建議，我們將確保您的特別日子充滿難忘的瞬間。
+        </p>
+        <!-- <a href="#" class="btn btn-primary btn-lg">立即探索</a> -->
+        </div>
+    </div>
+    </div>
+
+    <!-- 服務部分 -->
+    <div class="container my-5">
+        <div class="row text-center">
+            <div class="col-md-4">
+                <i class="bi bi-geo-alt-fill display-3 text-primary mb-3"></i>
+                <h3>驚人的場地</h3>
+                <p>我們為您挑選了一系列壯麗的場地，提升您的婚禮日體驗。</p>
+                <a href="#" class="btn btn-outline-secondary">預訂參觀</a>
+            </div>
+            <div class="col-md-4">
+                <i class="bi bi-calendar-check-fill display-3 text-primary mb-3"></i>
+                <h3>個性化規劃</h3>
+                <p>我們確保您的慶典完美反映您的獨特風格和願景。</p>
+            </div>
+            <div class="col-md-4">
+                <i class="bi bi-people-fill display-3 text-primary mb-3"></i>
+                <h3>婚禮商家推選</h3>
+                <p>精選優質婚禮商家，助您一站式解決所有需求，讓您的婚禮更無憂。</p>
+            </div> 
+        </div>
+    </div>
+
+    <!-- 婚禮企劃 -->
+    <div class="container my-5">
+    <div class="row">
+        <div class="col-md-6 order-1">
+            <img src="/src/assets/images/navbg/navImage.jpg" alt="婚禮" class="img-fluid rounded">
+        </div> 
+        <div class="col-md-6 order-0">
+            <h2 class="display-4">婚禮企劃書</h2>
+            <h3>精心策劃，隨心所欲</h3>
+            <p class="lead">
+                我們的婚禮企劃書將為您提供個性化的婚禮藍圖，幫助您在每一步中做出明智的選擇。從主題設計到細節執行，讓您的夢想婚禮成為現實。
+            </p>
+        </div>          
+    </div>
+    </div>
+
+    <!-- 場佈模擬器 -->
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 order-0">
+                <img src="/src/assets/images/navbg/navImage.jpg" alt="婚禮" class="img-fluid rounded">
+            </div>
+            <div class="col-md-6 order-1">
+                <h2 class="display-4">場景模擬器</h2>
+                <h3>一鍵預覽您的夢想婚禮</h3>
+                <p class="lead">
+                    利用我們的場景模擬器，您可以視覺化婚禮場地的每個細節，從佈置到色彩搭配，提前體驗最完美的婚禮氛圍，讓您的計劃更具信心。
+                </p>
+            </div>          
+        </div>
+    </div>
+
+
+    <!-- 預算規劃 -->
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 order-1">
+                <img src="/src/assets/images/navbg/navImage.jpg" alt="婚禮" class="img-fluid rounded">
+            </div> 
+            <div class="col-md-6 order-0">
+                <h2 class="display-4">預算規劃</h2>
+                <h3>輕鬆掌控每一分預算</h3>
+                <p class="lead">
+                    我們的預算規劃工具將幫助您清晰地了解每項開支，合理配置資金，確保您的婚禮不僅美好，還能在預算內完美實現您的願景。
+                </p>
+            </div>          
+        </div>
+    </div>
+
+
+    <!-- 婚禮流程規劃 -->
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 order-0">
+                <img src="/src/assets/images/navbg/navImage.jpg" alt="婚禮" class="img-fluid rounded">
+            </div> 
+            <div class="col-md-6 order-1">
+                <h2 class="display-4">婚禮流程規劃</h2>
+                <h3>每一刻都不容錯過</h3>
+                <p class="lead">
+                    我們的婚禮流程規劃服務將協助您設計詳細的時間表，從儀式到宴會，確保每個環節流暢進行，讓您在大日子裡無後顧之憂，享受每一個珍貴的瞬間。
+                </p>
+            </div>
+                    
+        </div>
+    </div>
+
+
+    <!-- 婚禮商家展示 -->
+    <div class="container my-5">
+        <h2 class="display-4 text-center mb-4">婚禮商家推選</h2>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card mb-4">
+                    <img src="/src/assets/images/homeImage/婚宴會場.jpg" class="card-img-top images-size" alt="婚宴會場">
+                    <div class="card-body">
+                        <h5 class="card-title">婚宴會場</h5>
+                        <p class="card-text">選擇我們精選的婚宴會場，為您的婚禮創造完美的氛圍。</p>
+                        <a href="#" class="btn btn-outline-secondary">了解更多</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mb-4">
+                    <img src="/src/assets/images/homeImage/婚宴菜餚.jpg" class="card-img-top images-size" alt="婚宴菜餚">
+                    <div class="card-body">
+                        <h5 class="card-title">婚宴菜餚</h5>
+                        <p class="card-text">我們提供多樣化的婚宴菜餚選擇，滿足每位賓客的味蕾。</p>
+                        <a href="#" class="btn btn-outline-secondary">了解更多</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mb-4">
+                    <img src="/src/assets/images/homeImage/禮車.jpg" class="card-img-top images-size" alt="禮車">
+                    <div class="card-body">
+                        <h5 class="card-title">禮車</h5>
+                        <p class="card-text">豪華禮車服務，為您和賓客提供舒適的接送體驗。</p>
+                        <a href="#" class="btn btn-outline-secondary">了解更多</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mb-4">
+                    <img src="/src/assets/images/homeImage/喜餅.jpg" class="card-img-top images-size" alt="喜餅">
+                    <div class="card-body">
+                        <h5 class="card-title">喜餅</h5>
+                        <p class="card-text">精美的喜餅選擇，讓您的婚禮更添甜蜜。</p>
+                        <a href="#" class="btn btn-outline-secondary">了解更多</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 行動呼籲 -->
+    <div class="container text-center my-5">
+    <h2>永恆的優雅，個性化的完美</h2>
+    <p>從挑選完美的供應商團隊到協調每一個細節，我們將確保您的婚禮慶典完美展現您的願景。</p>
+    <a href="#" class="btn btn-primary btn-lg">探索更多</a>
+    </div>
+
+    </div>
+
+</template>
+
+<style lang="css" scoped>
+    @font-face {
+    font-family: 'ChenYuluoyan-Thin'; /* 自定義字體名稱 */
+    src: url('@/assets/fonts/ChenYuluoyan-Thin.ttf') format('truetype'); /* 字體檔案路徑 */
+    font-weight: normal;
+    font-style: normal;
+    }
+
+    h1, h2, h3 {
+        font-family: 'ChenYuluoyan-Thin', sans-serif; /* 將自定義字體應用於標題、段落、按鈕等 */
+    }
+
+    .images-size {
+    width: 100%; /* 讓圖片寬度填滿容器 */
+    height: 200px; /* 設定固定高度 */
+    object-fit: cover; /* 確保圖片不變形並裁切至填滿 */
+    }
+
+    .card-title {
+        color: #333; 
+        font-weight: bold; 
+    }
+
+</style>
