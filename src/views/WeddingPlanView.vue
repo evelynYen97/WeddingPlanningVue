147c181567e013f1a22d3cb41528b40a19a70ad4
evelyn <evelyn0401evelyn@gmail.com>
2024-10-23 10:38:03 +0800

    pdf 及響應式内容初步測試,修改編輯器及内容排版,導入jsPDF及hrml2canvas

--- src/views/WeddingPlanView.vue
+++ src/views/WeddingPlanView.vue
@@ -3,27 +3,27 @@
     <div class="row">
       <div class="col-12 col-md-12 mb-3 mt-5">
         <h2>婚禮企劃書</h2>
+        <button @click="generatePDF">生成 PDF</button>
       </div>
     </div>
     <div class="row">
     <div class="block" >
-        <div id="quill-1">
         <button class="closeBtn mb-3">x</button>
-        <div class="quill-editor pdf-content" id="edit1" ref="pdfContent">
-            <h1 style="color: white; font-weight: bold;">xxx 與 xxx 的 婚禮主題</h1>
-        </div>
-            <button class="upBtn rounded">↑</button>
-            <button class="downBtn rounded">↓</button>
-            <div style="position: absolute;width: 1140px;height: 140px;bottom:0px;background: #001733;" class="pdf-content" ref="pdfContent"></div>
-             <div class="quill-editor" style="position:absolute;width:500px;height: 140px;bottom:0px; left: 50%;">
+        <button class="upBtn rounded">↑</button>
+        <button class="downBtn rounded">↓</button>
+        <div id="quill-1" ref="pdfContent1">
+            <h1 style="color: white; font-weight: bold;" id="edit1">xxx 與 xxx 的 婚禮主題</h1>
+        
+            <div style="position: absolute;width: 1140px;height: 140px;bottom:0px;background: #001733;"  ></div>
+             <div class="quill-editor" style="position:absolute;width:500px;height: 70px;bottom:50px; left: 50%;">
                 <h1 style="color: white; font-weight: bold;">愛的旅程，從此啟程。</h1>
-                <h2 style="color: white; font-weight: bold;">&nbsp;2024.10.24(婚禮日期)</h2>
              </div>
+                <h2 style="color: white; font-weight: bold;position:absolute;height:70px;bottom:0px; right: 10%;" class="mb-0">&nbsp;2024.10.24(婚禮日期)</h2>
             </div>
         </div>
     <div class="block">
         <button class="closeBtn">×</button>
-        <div style="width: 1200px;height:800px;background-color: #001733;" class="pdf-content" ref="pdfContent">
+        <div style="width: 1200px;height:800px;background-color: #001733;" ref="pdfContent2">
              <div class="quill-editor" id="edit2">
                 <p>
                 <img src="@/assets/images/weddingPlanImg/male.jpg" alt="男方" style="width:250px;">
@@ -41,7 +41,7 @@
                 </h4>
              </div>
              <img src="@/assets/images/weddingPlanImg/ring.png" alt="ring" id="ringImg">
-             <div class="quill-editor" id="edit4">
+             <div id="edit4">
                 <h4 style="color: white;">婚禮雙方介紹：</h4>
                 <h6 style="color: white;">&nbsp;尊敬的各位來賓，大家好！今天我們聚集在這裡，共同見證一對新人邁向人生新篇章的美好時刻。</h6>
                 <h6 style="color: white;">今天的男主角——張偉。他是一位才華洋溢的工程師，擁有穩定的職業和無限的創意。在生活中，他熱愛戶外運動，喜歡旅行，經常與朋友們分享他的冒險故事。他的善良和幽默感總能帶給周圍的人無限的快樂。</h6>
@@ -53,20 +53,19 @@
     </div>
     <div class="block">
         <button class="closeBtn">×</button>
-        <div id="blackUp" class="pdf-content" ref="pdfContent">
+        <div id="blackUp"  ref="pdfContent3">
             <div class="quill-editor" style="position:absolute; top: 100px;left: 700px;height: 80px;width: 300px;">
             <h2 style="color: white;font-weight: bold; position:absolute; top: 100px;left: 700px;">婚禮預算分配比例</h2>
         </div>
             <img src="@/assets/images/weddingPlanImg/Vector 1.png" alt="blackUp">
+            <img src="@/assets/images/weddingPlanImg/PieChart.png" alt="pieChart" id="PieChart">
         <div class="quill-editor" id="edit5">
-            <img src="@/assets/images/weddingPlanImg/PieChart.png" alt="pieChart">
             <br>
             <h4 style="color: grey; font-weight: bold;">婚禮預算分配比例</h4>
             <h5 style="color: grey; font-weight: bold;">預算分配備注</h5>
             <h5 style="color: grey; font-weight: bold;">1.場地花了...</h5>
             <h5 style="color: grey; font-weight: bold;">2.</h5>
             <h5 style="color: grey; font-weight: bold;">3.</h5>
-            <h5 style="color: grey; font-weight: bold;">4.</h5>
         </div>
         <div class="quill-editor" id="edit6">
             <h5 style="color: grey; font-weight: bold;">自由貼入想要放的預算相關圖片，或加入一些文字説明。</h5>
@@ -107,7 +106,48 @@
 </template>
   <script setup>
   import { onMounted, ref } from 'vue';
+    import jsPDF from 'jspdf';
+    import html2canvas from 'html2canvas';
   import Quill from 'quill'; 
+
+  //pdf
+   const pdfContent1 = ref(null);
+   const pdfContent2 = ref(null);
+   const pdfContent3 = ref(null);
+
+   const generatePDF = () => {
+  const elements = [pdfContent1.value, pdfContent2.value, pdfContent3.value];
+  const promises = elements.map(element => html2canvas(element));
+
+  Promise.all(promises).then(canvases => {
+    const pdf = new jsPDF();
+    const imgWidth = 190; // PDF 宽度
+    const pageHeight = pdf.internal.pageSize.height;
+
+    let position = 0; // 初始化位置
+    let count = 0; // 计数器
+
+    canvases.forEach(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      // 添加图片到 PDF
+      pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+      position += imgHeight; // 更新位置
+      count++; // 增加计数
+
+      // 每添加两个内容，就换一页
+      if (count % 2 === 0) {
+        pdf.addPage(); // 添加新页面
+        position = 0; // 重置位置
+      }
+    });
+
+    pdf.save('download.pdf');
+  });
+};
+
+
   onMounted(()=>{
     const editors = document.querySelectorAll('.quill-editor');
         editors.forEach(editor => {
@@ -166,6 +206,7 @@
          background: url('@/assets/images/weddingPlanImg/wed1.jpg') no-repeat;
           background-size: contain;
           height:800px;
+          width: 1200px;
       }
       #edit1{
         position: absolute;
@@ -216,7 +257,13 @@
         #edit5{
             position: absolute;
             width: 500px;
-            height: 650px;
+            height: 200px;
+            top:550px;
+            left: 100px;
+        }
+
+        #PieChart{
+            position: absolute;
             top: 100px;
             left: 100px;
         }
